31800b951efc40ec4abcff6df6da87a2d5fa47c8
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2026-01-29 12:09:03 +0300

    adds config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from datetime import timedelta
+
+
+#loads environment variables from .env file
+
+
+load_dotenv()
+print(os.getenv('DATABASE_URL'))
+
+class config:
+    #database
+    SQLALCHEMY_DATABASE_URI =os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    #Security
+    SECRET_KEY=os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=24)
+
+    #claude
+    ANTHROPIC_API_KEY=os.getenv('ANTHROPIC_API_KEY')
+
